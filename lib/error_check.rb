344755d1b4d4abf547d5e86760fe0efdedace26b
jstloyal <jstloyalty@gmail.com>
2020-05-08 15:37:36 +0100

    add initialize method to Error_check class

--- lib/error_check.rb
+++ lib/error_check.rb
@@ -1,4 +1,16 @@
 class Error_check
-
+  attr_reader :error_messages, :file_name, :valid
+  
+def initialize(file_name)
+  @file_name = file_name
+  @error_messages = []
+  @valid = true
+  begin
+    file_data = File.read(file_name).split
+  rescue StandardError
+    @valid = false
+  end
+  check_errors(file_data) if @valid
+end
   
 end
