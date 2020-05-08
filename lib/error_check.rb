ed7b93e5a1518f2abcaf6f7ac3acadd839c1cb8f
jstloyal <jstloyalty@gmail.com>
2020-05-08 15:42:21 +0100

    add check_errors method

--- lib/error_check.rb
+++ lib/error_check.rb
@@ -1,16 +1,25 @@
 class Error_check
   attr_reader :error_messages, :file_name, :valid
   
-def initialize(file_name)
-  @file_name = file_name
-  @error_messages = []
-  @valid = true
-  begin
-    file_data = File.read(file_name).split
-  rescue StandardError
-    @valid = false
+  def initialize(file_name)
+    @file_name = file_name
+    @error_messages = []
+    @valid = true
+    begin
+      file_data = File.read(file_name).split
+    rescue StandardError
+      @valid = false
+    end
+    check_errors(file_data) if @valid
   end
-  check_errors(file_data) if @valid
-end
+
+  def check_errors(file_data)
+    check_indentation(file_data)
+    check_trailing_space(file_data)
+    check_double_space(file_data)
+    check_double_quotes(file_data)
+    check_new_line(file_data)
+  end
+  
   
 end
